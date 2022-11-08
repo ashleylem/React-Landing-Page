40c934f05ef9e3fac12a3893c9dd2f2ca273186f
ashleylem <ashley.lemus@yahoo.com>
2022-11-08 16:37:36 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,67 @@
 import React from "react";
+import PropType from "prop-types";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+export const Navbar=()=>{
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+		<div className="container-fluid w-75">
+		  <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+		  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		  </button>
+		  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div className="navbar-nav pt-2">
+			  <a className="nav-link text-light active" aria-current="page" href="#">Home</a>
+			  <a className="nav-link text-light" href="#">About</a>
+			  <a className="nav-link text-light" href="#">Services</a>
+			  <a className="nav-link text-light ">Contact</a>
+			</div>
+		  </div>
 		</div>
-	);
+	  </nav>);
 };
 
-export default Home;
+export const Jumbotron =()=>{
+	return(
+		<div className= "container-fluid bg-secondary bg-opacity-25 mt-3 rounded w-75">
+			<div className= "col-12 ">
+				<h1>Dogs: man's best friend</h1>
+				<p>Dogs can have many positive effects on the lives of their owners. They influence social, emotional, and cognitive development in children, promote an active lifestyle, provide companionship, and have even been able to detect oncoming epileptic seizures or the presence of certain cancers. Dogs can also help to relieve stress and anxiety in people.</p>
+				<a href="https://www.petfinder.com/" target="_blank" className="btn btn-primary mb-5">Rescue Dogs!</a>
+			</div>
+		</div>
+	)
+}
+ export const Card = (props)=>{
+	return (
+		<div className="card mx-2 border">
+  				<img src={props.imageUrl} className="card-img-top img-fluid " alt="..." />
+  					<div className="card-body">
+    				<h5 className="card-title">{props.title}</h5>
+    				<p className="card-text">{props.description}</p>
+					</div>
+					<div className="card-footer">
+    				<a href={props.buttonUrl}  target="_blank" className="btn ms-5 btn-primary">{props.buttonLabel}</a>
+  					</div>
+			
+		</div>
+	)
+ }
+
+Card.PropType={
+	title: PropType.string,
+	description: PropType.string,
+	imageUrl: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string
+}
+
+
+export const CardCreator=(title, description, image, buttonUrl, buttonLabel)=> {
+	return (<Card title= {title} description= {description} imageUrl= {image} buttonUrl= {buttonUrl} buttonLabel= {buttonLabel}
+		/>)
+	}
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,42 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import {Navbar, Jumbotron, Card, CardCreator} from "./component/home.jsx";
+
+let card1 = (CardCreator(
+	"Golden Retriever", 
+	"Golden Retrievers are kind, trusting, and gentle. Goldens are outgoing, trustworthy, and eager-to-please family dogs, and relatively easy to train. They take a joyous and playful approach to life and maintain this puppyish behavior into adulthood. These energetic, powerful gundogs enjoy outdoor play. For a breed built to retrieve waterfowl for hours on end, swimming and fetching are natural pastimes.  ",
+	"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg",
+	"https://www.rover.com/blog/breeds/golden-retriever/",
+	"Find Out More!",
+))
+let card2 = (CardCreator(
+	"Bernese Mountain Dog", 
+	"Big, powerful, and built for hard work, the Bernese Mountain Dog is also strikingly beautiful and blessed with a sweet, affectionate nature. Berners are generally placid but are always up for a romp with the owner, whom they live to please. Berners get along with the entire family and are particularly gentle with children, but they will often become more attached to one lucky human. Berners are imposing but not threatening, and they maintain an aloof dignity with strangers.",
+	"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F05%2F27%2Fbernese-mountain-dog-sitting-puppy-204356895-2000.jpg",
+	"https://www.akc.org/dog-breeds/bernese-mountain-dog/",
+	"Find Out More!",
+))
+
+let card3 = (CardCreator(
+	"Siberian Husky", 
+	"Having a Husky means checking your locks to make sure they can’t escape. Their desire to explore and roam the outside world can be a challenge to keep in a box, and they’ll discover all sorts of ways to break out of your house or yard. (Is the trash can far enough way from the fence?) You’ll just have to give in and hit the trails with them. This delightfully smart dog loves adventure, and they prefer to have them with you.",
+	"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F06%2F14%2Fsiberian-husky-puppy-grass-146571433-2000.jpg",
+	"https://be.chewy.com/dog-breed/husky/",
+	"Find Out More!",
+))
+let card4 = (CardCreator(
+	"Yorkie", 
+	"Yorkies are one of the smallest dog breeds, measuring just 7-10” tall with adults weighing no more than seven pounds. They’re known for appearing confident thanks to their high head carriage and compact, well-proportioned body shape. Yorkies are low-slung with a straight back, and have a small head, carried high, with v-shaped ears that stand up tall. ",
+	"https://www.rover.com/blog/wp-content/uploads/2021/06/aurora.the_.amazing.yorkie-1024x1019.jpg",
+	"https://www.rover.com/blog/breeds/yorkshire-terrier/",
+	"Find Out More!",
+))
+
+const theCards= React.createElement('div', {class:  "card-group w-75 px-0 mt-3 container-fluid mb-4 gy-3 "}, [card1, card2, card3, card4]);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar/>, document.querySelector("#navbar"));
+ReactDOM.render(<Jumbotron/>, document.querySelector("#jumbotron"))
+ReactDOM.render(theCards, document.querySelector("#cards"))
+
